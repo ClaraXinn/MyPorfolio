1a00d9c0f735a95c457bf593566871dba65190a2
ClaraXinn <claraxin8@gmail.com>
2025-10-08 18:25:21 +0800

    my approach session added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
@@ -20,6 +21,7 @@ export default function Home() {
         <Grid/>
         <RecentProjects/>
         <Experience/>
+        <Approach/>
   
 
       </div>
